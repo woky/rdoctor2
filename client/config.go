9d76df6c8902f90776b9744a3333fd416f31f555
Tomáš Virtus <tomas.virtus@rchain.coop>
2018-12-17 08:56:26 +0100

    Refactor config
    - Get entries from environment too (RDOCTOR_ prefix)
    - Add lot of accessors

--- client/config.go
+++ client/config.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"net/url"
+	"os"
 	"os/user"
 	"path"
+	"strings"
+
 	//"github.com/go-ini/ini"
 	"gopkg.in/ini.v1"
 )
@@ -13,27 +17,102 @@ type Config struct {
 	mainSection *ini.Section
 }
 
-func (conf *Config) getString(key string) (value string, present bool) {
-	present = conf.mainSection.HasKey(key)
-	if present {
-		value = conf.mainSection.Key(key).String()
-	}
-	return
-}
-
-func (conf *Config) setString(key string, value string) {
-	conf.mainSection.Key(key).SetValue(value)
-}
-
 func (conf *Config) Location() string {
 	return conf.location
 }
-func (conf *Config) GetApiKey() (string, bool) {
-	return conf.getString("apikey")
+
+func getFromEnv(key string) (val string, present bool) {
+	varName := "RDOCTOR_" + strings.ToUpper(key)
+	val, present = os.LookupEnv(varName)
+	return
+}
+
+func (conf *Config) Has(key string) bool {
+	if conf.mainSection.HasKey(key) {
+		return true
+	}
+	_, present := getFromEnv(key)
+	return present
+}
+
+func (conf *Config) Get(key string) string {
+	if conf.mainSection.HasKey(key) {
+		return conf.mainSection.Key(key).String()
+	}
+	val, present := getFromEnv(key)
+	if !present {
+		panic("Configuration key '" + key + "' not set!")
+	}
+	return val
+}
+
+func (conf *Config) GetOrElse(key string, def string) string {
+	if conf.Has(key) {
+		return conf.Get(key)
+	}
+	return def
+}
+
+func (conf *Config) GetBool(key string, def bool) bool {
+	if !conf.Has(key) {
+		return def
+	}
+	val := conf.Get(key)
+	return !(val == "0" || strings.EqualFold(val, "no") ||
+		strings.EqualFold(val, "false"))
+}
+
+func (conf *Config) HasApiKey() bool {
+	return conf.Has("apikey")
+}
+
+func (conf *Config) GetApiKey() string {
+	return conf.Get("apikey")
 }
 
 func (conf *Config) SetApiKey(value string) {
-	conf.setString("apikey", value)
+	conf.mainSection.Key("apikey").SetValue(value)
+}
+
+func (conf *Config) GetServiceAddress() string {
+	return conf.GetOrElse("address", "rdoctor.rchain-dev.tk")
+}
+
+func (conf *Config) IsServiceSecure() bool {
+	return conf.GetBool("secure", true)
+}
+
+func (conf *Config) GetServiceHttpUrl(path string) string {
+	proto := "https://"
+	if !conf.IsServiceSecure() {
+		proto = "http://"
+	}
+	return proto + conf.GetServiceAddress() + path
+}
+
+func (conf *Config) GetServiceWsUrl(path string) string {
+	proto := "wss://"
+	if !conf.IsServiceSecure() {
+		proto = "ws://"
+	}
+	return proto + conf.GetServiceAddress() + path
+}
+
+func (conf *Config) GetNewKeyUrl(identity string) string {
+	query := url.Values{"identity": {identity}}
+	return conf.GetServiceHttpUrl("/api/newkey?" + query.Encode())
+}
+
+func (conf *Config) GetConfirmKeyUrl(apiKey string) string {
+	return conf.GetServiceHttpUrl("/confirm?key=" + apiKey)
+}
+
+func (conf *Config) GetPingUrl() string {
+	return conf.GetServiceHttpUrl("/api/ping?key=" + conf.GetApiKey())
+}
+
+func (conf *Config) GetSubmitLogUrl() string {
+	return conf.GetServiceWsUrl("/api/ws/submitlog?key=" + conf.GetApiKey())
 }
 
 func (conf *Config) Save() {
