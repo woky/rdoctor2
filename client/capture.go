93c03d55642c5a9ae2fd001176b89296bffa900c
Tomáš Virtus <tomas.virtus@rchain.coop>
2018-12-17 04:09:27 +0100

    Use os/exec to spawn program and read its outputs

--- client/capture.go
+++ client/capture.go
@@ -1,22 +1,14 @@
 package main
 
-/*
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-*/
-import "C"
-
 import (
 	"bufio"
+	"context"
 	"fmt"
-	"golang.org/x/sys/unix"
 	"io"
 	"os"
-	"strings"
+	"os/exec"
+	"os/signal"
 	"syscall"
-	"time"
 )
 
 type CapturedLine struct {
@@ -31,161 +23,93 @@ func (c CapturedLine) String() string {
 	if c.Stderr {
 		origin = "STDERR"
 	}
-	format := "%s L%03d \"%s\""
+	format := "%s L%03d: %s"
 	if c.Eof {
-		format = "%s L%03d EOF \"%s\""
+		format = "%s L%03d EOF: %s"
 	}
 	return fmt.Sprintf(format, origin, c.LineNumber, c.Line)
 }
 
-func captureOutput(fd uintptr, linesChan chan CapturedLine, stderr bool) {
-	defer close(linesChan)
-	_, err := unix.FcntlInt(fd, unix.F_SETFL, unix.O_NONBLOCK)
-	if err != nil {
-		Die("Could not make file descriptor non-blocking: %s", err)
+func readLines(pipe io.ReadCloser, lines chan CapturedLine, stderr bool) {
+	defer pipe.Close()
+	defer close(lines)
+	copyOut := os.Stdout
+	if stderr {
+		copyOut = os.Stderr
 	}
-	file := os.NewFile(fd, "")
-	defer file.Close()
-	reader := bufio.NewReader(file)
-	var lineNumber uint64 = 1
+	scanner := bufio.NewScanner(pipe)
+	var lineNumber uint64 = 0
 	for {
-		line, err := reader.ReadString('\n')
-		line = strings.TrimRight(line, "\r\n")
-		eof := false
-		if err != nil {
-			if err != io.EOF {
-				SayErr("Could not read from pipe: %s", err)
-				break
-			}
-			eof = true
-		}
+		eof := !scanner.Scan()
 		capturedLine := CapturedLine{
-			Line:       line,
+			Line:       scanner.Text(),
 			LineNumber: lineNumber,
 			Stderr:     stderr,
 			Eof:        eof,
 		}
-		linesChan <- capturedLine
+		fmt.Fprintln(copyOut, capturedLine.Line)
+		lines <- capturedLine
 		if eof {
 			break
 		}
 		lineNumber++
 	}
-	SayErr("Exit")
+	if err := scanner.Err(); err != nil {
+		SayErr("Could not read from pipe: %s", err)
+	}
 }
 
-//func captureOutput(fd uintptr, linesChan chan CapturedLine, stderr bool) {
-//	defer close(linesChan)
-//	_, err := unix.FcntlInt(fd, unix.F_SETFL, unix.O_NONBLOCK)
-//	if err != nil {
-//		Die("Could not make file descriptor non-blocking: %s", err)
-//	}
-//	file := os.NewFile(fd, "")
-//	defer file.Close()
-//	scanner := bufio.NewScanner(file)
-//	var lineNumber uint64 = 1
-//	for {
-//		SayOut("Reading")
-//		eof := !scanner.Scan()
-//		capturedLine := CapturedLine{
-//			Line:       scanner.Text(),
-//			LineNumber: lineNumber,
-//			Stderr:     stderr,
-//			Eof:        eof,
-//		}
-//		SayOut("Writing to chan")
-//		linesChan <- capturedLine
-//		if eof {
-//			break
-//		}
-//		lineNumber++
-//	}
-//	if err := scanner.Err(); err != nil {
-//		SayErr("Could not read from pipe: %s", err)
-//	}
-//	SayErr("Exit")
-//}
-
-func captureBoth(stdoutFd uintptr, stderrFd uintptr, linesChan chan CapturedLine) {
-	stdoutLinesChan := make(chan CapturedLine)
-	stderrLinesChan := make(chan CapturedLine)
-	go captureOutput(stdoutFd, stdoutLinesChan, false)
-	go captureOutput(stderrFd, stderrLinesChan, true)
+func combineOutputs(stdout, stderr io.ReadCloser, lines chan CapturedLine) {
+	defer close(lines)
+	stdoutLines := make(chan CapturedLine)
+	stderrLines := make(chan CapturedLine)
+	go readLines(stdout, stdoutLines, false)
+	go readLines(stderr, stderrLines, true)
 	for {
 		select {
-		case line, ok := <-stdoutLinesChan:
+		case line, ok := <-stdoutLines:
 			if ok {
-				linesChan <- line
+				lines <- line
 			} else {
-				stdoutLinesChan = nil
+				stdoutLines = nil
 			}
-		case line, ok := <-stderrLinesChan:
+		case line, ok := <-stderrLines:
 			if ok {
-				linesChan <- line
+				lines <- line
 			} else {
-				stderrLinesChan = nil
+				stderrLines = nil
 			}
 		}
-		if stdoutLinesChan == nil && stderrLinesChan == nil {
-			close(linesChan)
+		if stdoutLines == nil && stderrLines == nil {
 			break
 		}
 	}
 }
 
-func ForkMainProgram(cmdLine []string, linesChan chan CapturedLine) {
-	var stdoutPipe, stderrPipe [2]int
+func StartMainProgram(cmdLine []string, lines chan CapturedLine) {
 	var err error
-	var retInt _Ctype_int
-
-	if len(cmdLine) == 0 {
-		panic("Empty cmdLine")
-	}
-
-	err = syscall.Pipe(stdoutPipe[:])
+	ctx, cancel := context.WithCancel(context.Background())
+	cmd := exec.CommandContext(ctx, cmdLine[0], cmdLine[1:]...)
+	var stdout, stderr io.ReadCloser
+	stdout, err = cmd.StdoutPipe()
 	if err != nil {
-		Die("Could not create Unix pipe: %s", err)
+		Die("Could not create pipe: %s", err)
 	}
-	err = syscall.Pipe(stderrPipe[:])
+	stderr, err = cmd.StderrPipe()
 	if err != nil {
-		Die("Could not create Unix pipe: %s", err)
+		Die("Could not create pipe: %s", err)
 	}
-
-	retInt, err = C.fork()
-	if retInt == -1 {
-		Die("Could not fork current process: %s", err)
-		os.Exit(1)
-	}
-	childPid := int(retInt)
-
-	if childPid == 0 {
-		time.Sleep(1 * time.Second)
-		SayOut("Reading...")
-		syscall.Close(stdoutPipe[1])
-		syscall.Close(stderrPipe[1])
-		os.Stdin.Close()
-		go captureBoth(uintptr(stdoutPipe[0]), uintptr(stderrPipe[0]), linesChan)
-		return
-	}
-
-	// childPid > 0
-	syscall.Close(stdoutPipe[0])
-	syscall.Close(stderrPipe[0])
-	os.Stdout.Close()
-	os.Stderr.Close()
-	err = syscall.Dup2(stdoutPipe[1], 1)
+	go combineOutputs(stdout, stderr, lines)
+	interrupts := make(chan os.Signal)
+	signal.Notify(interrupts, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
+	go func() {
+		for {
+			<-interrupts
+			cancel()
+		}
+	}()
+	err = cmd.Start()
 	if err != nil {
-		Die("Could not duplicate file descriptor: %s", err)
+		Die("Could not create process: %s", err)
 	}
-	err = syscall.Dup2(stderrPipe[1], 2)
-	if err != nil {
-		Die("Could not duplicate file descriptor: %s", err)
-	}
-	os.Stdout = os.NewFile(uintptr(1), "/dev/stdout")
-	os.Stderr = os.NewFile(uintptr(2), "/dev/stderr")
-	err = syscall.Exec(cmdLine[0], cmdLine, os.Environ())
-	if err != nil {
-		Die("Could not execute wrapped program: %s", err)
-	}
-	panic("Successful return from exec()")
 }
